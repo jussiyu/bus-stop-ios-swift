add5500295022cd04edee1a850771fe1e493e6ad
Jussi Yli-Urpo <jyliurpo@gmail.com>
2015-08-13 16:09:00 +0300

    Ignore and stop tracking Pods.

--- Pods/Target Support Files/MediumProgressView/MediumProgressView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MediumProgressView : NSObject
-@end
-@implementation PodsDummy_MediumProgressView
-@end
--- Pods/Target Support Files/Pods-BusStopUnitTests/Pods-BusStopUnitTests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_BusStopUnitTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_BusStopUnitTestsVersionString[];
-
